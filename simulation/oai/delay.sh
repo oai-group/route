137e123eb753aceecf60085ea0004780ca68bc0d
zcq98 <1229896069@qq.com>
2022-06-24 15:56:56 +0800

    measure delay and loss

---
+++ simulation/oai/delay.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Shell script absolute path 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "当前脚本所在路径为: $DIR"
+cd $DIR
+cd ..
+
+# Check if .py38 has been built
+py38=$(ls -a ./|grep .py38)
+if [ $py38 ]; then  
+  echo "Python3虚拟环境目录已安装"
+else
+  echo "请先运行init.sh创建Python3虚拟环境并下载依赖"
+  exit
+fi
+
+# Enter Python3 virtual environment
+source ./.py38/bin/activate
+source ./python.rc
+
+# Check if namespace if h9 exists
+h9=$(ip netns|grep h9)
+if [-z $h9]; then  
+  echo "命名空间h9不存在,请先运行topo.sh和manage.sh生成回传网络和Host"
+  exit
+fi
+ip netns exec h9 python telemetry/sniffer.delay.py
