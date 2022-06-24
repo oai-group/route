892f469c6601ff2063d099ab04f2a2bc86f4f0ea
zcq98 <1229896069@qq.com>
2022-06-24 14:56:49 +0800

    clear all script

---
+++ simulation/clear_all.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+for bridge in `ovs-vsctl list-br`; 
+do
+                ovs-vsctl del-br $bridge
+                echo "$bridge" deleted
+done
+
+for hid in {0..24}
+do
+    ip netns del "h${hid}"
+done
+
+ip netns del test
+
+
+for name in $(ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' | grep "-")
+do
+    echo $name
+    ip link del dev ${name}
+done
+
+
+ip link del dev nat1
+ip link del dev nat2
+
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+
+pkill -f '^gogen$'
+pkill -f '^golisten$'
+pkill "ovs-tcpdump"
+pkill "tcpdump"
+
+#service docker restart
