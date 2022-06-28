9d79710e9d78f8b4d5874e45564924f603665a6e
zcq98 <1229896069@qq.com>
2022-06-28 09:43:28 +0800

    check smartfwd and route algorithm

--- simulation/oai/delay.sh
+++ simulation/oai/delay.sh
@@ -15,6 +15,24 @@ else
   exit
 fi
 
+# Check smartfwd and route algorithm
+rm /root/.ssh/known_hosts
+active_apps=$(sshpass -p "rocks" ssh -o StrictHostKeyChecking=no -p 8101 onos@localhost "apps -s -a")
+smartfwd_state=$(echo $active_apps|xargs -n 1|grep org.onosproject.smartfwd)
+if [ $smartfwd_state ]; then  
+  echo "smartfwd模块安装并启动"
+else
+  echo "smartfwd模块安装失败,请先安装smartfwd模块!!!"
+  exit
+fi
+algorithm_instance=$(docker ps|grep algorithm_instance|awk '{print $1}')
+if [ $algorithm_instance ]; then
+  echo "route algorithm 容器已运行"
+else
+  echo "请先运行route algorithm 容器!!!"
+  exit
+fi
+
 # Enter Python3 virtual environment
 source ./.py38/bin/activate
 source ./python.rc
